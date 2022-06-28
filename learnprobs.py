6275c0a5f4300a32992ffe0819ebfa955856ec10
Narvin Chana <narvin.chana@etu.univ-tours.fr>
2022-06-28 17:49:38 +0200

    feat: tentative first implementation of the bayesian_dirichlet distribution

--- learnprobs.py
+++ learnprobs.py
@@ -1,3 +1,4 @@
+import math
 import sys
 
 import numpy
@@ -77,6 +78,60 @@ def frequentist(sample: np.ndarray, model: stormpy.SparseDtmc, smoothing: float 
     return row, col, values
 
 
+def bayesian_dirichlet(sample: np.ndarray, model: stormpy.SparseDtmc):
+    """
+    Estimates the transition probabilities using a dirichlet distribution via MAP-estimation.
+    :param sample: List of random observations on the model
+    :param model: Model whose transitions must be estimated
+    :return:
+    """
+    N_states = model.nr_states
+    N = np.zeros(N_states)
+    m = model.nr_transitions
+
+    # Create sparse-matrix representation
+    row = np.zeros(N_states + 1, numpy.int8)
+    for s in range(N_states):
+        row[s + 1] = row[s] + m[s]
+    col = []
+    for s in model.states:
+        for t in s.actions[0].transitions:
+            col.append(t.column)
+    col = np.array(col)
+
+    values = np.zeros(np.sum(m))
+    a = np.ones(m)
+    k = np.zeros([np.sum(m), np.sum(m)])
+
+    # Count N and k's values
+    for (start, dest) in sample:
+        N[start] += 1
+        i = row[start]
+        for j in range(i, row[start + 1]):
+            if col[j] == dest:
+                k[i, j] += 1
+
+    # this is the multinomial likelihood, no clue what it's even used for as we obtain the estimations with the mode lower down
+    for s in range(N_states):
+        for i in range(row[s], row[s + 1]):
+            values[i] = (N[s] - 1) / math.prod([math.factorial(j) for j in k[i]]) \
+                        * math.prod([math.pow(values[i], j) for j in k[i]])
+
+    rng = np.random.default_rng()
+    dirichlet_distribution = rng.dirichlet(a, m)
+
+    # this is supposed to be the posterior(p1,...,pm) ~ Dir(a1+k1,...,am+km)
+    # i made it assign its value to a, but no clue if that's correct
+    a = [dirichlet_distribution[a[i] + k[i]] for i in range(m)]
+
+    # estimate p with the mode
+    for s in range(N_states):
+        for i in range(row[s], row[s+1]):
+            values[i] = (a[i] - 1) / (sum(a) - m)
+
+    return row, col, values
+
+
 if __name__ == "__main__":
     program = stormpy.parse_prism_program(stormpy.examples.files.prism_dtmc_die)
     model = stormpy.build_model(program)
@@ -87,6 +142,15 @@ if __name__ == "__main__":
         method = sys.argv[1]
         if method == "frequentist":
             r, c, v = frequentist(obs, model)
+            m = model_from_sparse_matrix(r, c, v)
+
+            for state in m.states:
+                for action in state.actions:
+                    for transition in action.transitions:
+                        print(f"{state.id}, {transition.value()}, {transition.column}")
+
+        elif method == "bayesian":
+            r, c, v = bayesian_dirichlet(obs, model)
             m = model_from_sparse_matrix(r, c, v)
 
             for state in m.states:
