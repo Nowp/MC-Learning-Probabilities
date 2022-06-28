93390d89f0c1211c2bb092388995a3bde2b24ba1
Narvin Chana <narvin.chana@etu.univ-tours.fr>
2022-06-28 18:59:27 +0200

    fix: removed usage of newAlpha

--- learnprobs.py
+++ learnprobs.py
@@ -82,23 +82,22 @@ def bayesian_dirichlet(sample: np.ndarray, model: stormpy.SparseDtmc):
     Estimates the transition probabilities using a dirichlet distribution via MAP-estimation.
     :param sample: List of random observations on the model
     :param model: Model whose transitions must be estimated
-    :return:
     """
-    N_states = model.nr_states
-    N = np.zeros(N_states)
+    n_states = model.nr_states
+    N = np.zeros(n_states)
     m = model.nr_transitions
+    nb_trans = [len(s.actions[0].transitions) for s in model.states]
+    row = np.zeros(n_states + 1, numpy.int8)
+    for s in range(n_states):
+        row[s + 1] = row[s] + nb_trans[s]
 
-    # Create sparse-matrix representation
-    row = np.zeros(N_states + 1, numpy.int8)
-    for s in range(N_states):
-        row[s + 1] = row[s] + m[s]
     col = []
     for s in model.states:
         for t in s.actions[0].transitions:
             col.append(t.column)
     col = np.array(col)
 
-    values = np.zeros(np.sum(m))
+    values = np.zeros(np.sum(nb_trans))
     a = np.ones(m)
     k = np.zeros([np.sum(m), np.sum(m)])
 
@@ -108,27 +107,34 @@ def bayesian_dirichlet(sample: np.ndarray, model: stormpy.SparseDtmc):
         i = row[start]
         for j in range(i, row[start + 1]):
             if col[j] == dest:
-                k[i, j] += 1
+                k[i,j] += 1
 
     # this is the multinomial likelihood, no clue what it's even used for as we obtain the estimations with the mode lower down
-    for s in range(N_states):
+    for s in range(n_states):
         for i in range(row[s], row[s + 1]):
-            values[i] = (N[s] - 1) / math.prod([math.factorial(j) for j in k[i]]) \
+            values[i] = (N[s] - 1) / math.prod([math.factorial(int(j)) for j in k[i]]) \
                         * math.prod([math.pow(values[i], j) for j in k[i]])
 
     rng = np.random.default_rng()
     dirichlet_distribution = rng.dirichlet(a, m)
 
-    # this is supposed to be the posterior(p1,...,pm) ~ Dir(a1+k1,...,am+km)
-    # i made it assign its value to a, but no clue if that's correct
-    a = [dirichlet_distribution[a[i] + k[i]] for i in range(m)]
+    # this is supposed to be the posterior(p1,...,pm) ~ Dir(a1+k1,...,am+km), not sure what it is used for
+    newAlpha = a
+    for elem in k:
+        newAlpha += elem
+    new_dist = rng.dirichlet(newAlpha, m)
 
     # estimate p with the mode
-    for s in range(N_states):
-        for i in range(row[s], row[s+1]):
+    builder = stormpy.SparseMatrixBuilder(rows=n_states, columns=n_states)
+    for s in range(n_states):
+        for i in range(row[s], row[s + 1]):
+            #values[i] = (new_dist[i] - 1) / (sum(new_dist) - m)
             values[i] = (a[i] - 1) / (sum(a) - m)
+            c = col[i]
+            v = values[i]
+            builder.add_next_value(s, c, v)
 
-    return row, col, values
+    return builder.build()
 
 
 if __name__ == "__main__":
